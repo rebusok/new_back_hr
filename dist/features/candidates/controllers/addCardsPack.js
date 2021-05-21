6175d605247d707950f354ddeb3f37254750d9dc
Yuriy <rebusok@yandex.ru>
2021-05-21 09:24:13 +0300

    change add, update

---
+++ dist/features/candidates/controllers/addCardsPack.js
@@ -0,0 +1,67 @@
+"use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.addCandidatePack = void 0;
+const user_1 = __importDefault(require("../../auth/models/user"));
+const errorStatuses_1 = require("../../auth/helpers/errorStatuses");
+const candidatesPack_1 = __importDefault(require("../models/candidatesPack"));
+const cookie_1 = require("../../../main/cookie");
+exports.addCandidatePack = (req, res, user) => __awaiter(void 0, void 0, void 0, function* () {
+    const { candidatesPack } = req.body;
+    console.log(candidatesPack);
+    if (!candidatesPack)
+        errorStatuses_1.status400(res, "No cardsPack in body! /ᐠ-ꞈ-ᐟ\\", user, "addCardsPack", { body: req.body });
+    else {
+        const pathF = candidatesPack.path || "/def";
+        const typeF = candidatesPack.type || "pack";
+        const recommendationF = candidatesPack.recommendation || '';
+        candidatesPack_1.default.create({
+            user_id: user._id,
+            user_name: user.name,
+            name: candidatesPack.name,
+            path: pathF,
+            status: '',
+            more_id: user._id,
+            recommendation: recommendationF,
+            leaderInterview: null,
+            position: candidatesPack.position,
+            date: new Date(Date.parse(candidatesPack.date)).toISOString(),
+            time: candidatesPack.time,
+            phone: candidatesPack.phone,
+            meeting: null,
+            SS: null,
+            total: '',
+            type: typeF,
+            created: new Date(),
+            updated: new Date(),
+            _doc: {},
+        }).then((newCardsPack) => {
+            user_1.default.findById(user._id)
+                .exec()
+                .then((updatedUser) => {
+                if (!updatedUser)
+                    errorStatuses_1.status400(res, "not updated? /ᐠ｡ꞈ｡ᐟ\\", user, "addCardsPack");
+                else
+                    cookie_1.resCookie(res, user).status(201).json({
+                        newCardsPack,
+                        token: user.token,
+                        tokenDeathTime: user.tokenDeathTime,
+                    });
+            })
+                .catch(e => errorStatuses_1.status500(res, e, user, "addCardsPack/User.findByIdAndUpdate"));
+        })
+            .catch(e => errorStatuses_1.status500(res, e, user, "addCardsPack/CardsPack.create"));
+    }
+});
+//# sourceMappingURL=addCardsPack.js.map
---
+++ dist/features/candidates/controllers/getCandidatesCards.js
@@ -0,0 +1,72 @@
+"use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.getCandidatesCards = void 0;
+const candidatesPack_1 = __importDefault(require("../models/candidatesPack"));
+const cookie_1 = require("../../../main/cookie");
+const errorStatuses_1 = require("../../auth/helpers/errorStatuses");
+exports.getCandidatesCards = (req, res, user) => __awaiter(void 0, void 0, void 0, function* () {
+    const { sortPacks, packName, user_id, type, searchStatus, searchTotal, searchPosition } = req.query;
+    const sortPacksF = sortPacks || ""; // '0grade'
+    const packNameF = packName || "";
+    const searchStatusF = searchStatus || "";
+    const searchTotalF = searchTotal || "";
+    const searchPositionF = searchPosition || "";
+    console.log(user_id);
+    console.log(packName);
+    console.log(sortPacks);
+    console.log(searchStatus);
+    console.log(searchTotal);
+    console.log(searchPosition);
+    const user_idF = user_id || undefined;
+    // const typeF = type as string | undefined || "pack";
+    const user_idO = user_idF ? { user_id: user_idF } : undefined; // options
+    candidatesPack_1.default.findOne(user_idO)
+        .sort({ date: -1 }).exec()
+        .then((CandidatesPackDateMax) => {
+        // Сортировка по увеличению и уменьшению разбивка квери параметра
+        const sortName = (sortPacksF && sortPacksF.length > 2) ? sortPacksF.slice(1) : "";
+        const direction = sortName ? (sortPacksF[0] === "0" ? -1 : 1) : undefined;
+        const sortO = sortName ? { [sortName]: direction } : {};
+        console.log(searchPositionF);
+        const findBase = {
+            name: new RegExp(packNameF, "gi"),
+            status: new RegExp(`^${searchStatusF}`, "gi"),
+            // total: new RegExp(searchTotalF, "gi"),
+            position: new RegExp(searchPositionF, "gi"),
+        };
+        const findPrivate = user_idF && user._id.equals(user_idF) ? {} : { private: false };
+        const findByUserId = user_id ? { user_id: user_idF } : {};
+        const findO = Object.assign(Object.assign(Object.assign({}, findByUserId), findBase), findPrivate);
+        candidatesPack_1.default.find(findO)
+            .exec()
+            .then(totalCountCandidatesPack => {
+            const totalPacks = totalCountCandidatesPack.length;
+            candidatesPack_1.default.find(findO)
+                .sort(Object.assign(Object.assign({}, sortO), { updated: -1 }))
+                .lean()
+                .exec()
+                .then(candidatesPacks => {
+                cookie_1.resCookie(res, user).status(200)
+                    .json({
+                    candidatesPacks,
+                    totalPacks,
+                    token: user.token,
+                    tokenDeathTime: user.tokenDeathTime,
+                });
+            }).catch(e => errorStatuses_1.status500(res, e, user, "getCardPacks/CardsPack.find"));
+        }).catch(e => errorStatuses_1.status500(res, e, user, "getCardPacks/TotalCandidates"));
+    }).catch(e => errorStatuses_1.status500(res, e, user, "getCardPacks/CardsPack.findOne/min"));
+});
+//# sourceMappingURL=getCandidatesCards.js.map
